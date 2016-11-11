b421752001399a27344ed7cfecd1108e2879dacd
hizzlekizzle <hizzlekizzle@users.noreply.github.com>
2016-11-11 10:15:39 -0600

    change HSV/RGB conversion to fast, branchless version

--- misc/colorspace-tools.h
+++ misc/colorspace-tools.h
@@ -163,20 +163,23 @@ vec3 RGBtoHCV(vec3 RGB)
     return vec3(H, C, Q.x);
 }
   
-  vec3 RGBtoHSV(vec3 RGB)
-  {
-	vec3 HCV = RGBtoHCV(RGB);
-	float S = HCV.y / (HCV.z + 1e-10);
-	
-	return vec3(HCV.x, S, HCV.z);
-  }
-  
-  // Converting HSV to RGB
-  vec3 HSVtoRGB(vec3 HSV)
-  {
-    vec3 RGB = HUEtoRGB(HSV.x);
-    return ((RGB - 1.0) * HSV.y + 1.0) * HSV.z;
-  }
+vec3 RGBtoHSV(vec3 c)
+{
+    vec4 K = vec4(0.0, -1.0 / 3.0, 2.0 / 3.0, -1.0);
+    vec4 p = c.g < c.b ? vec4(c.bg, K.wz) : vec4(c.gb, K.xy);
+    vec4 q = c.r < p.x ? vec4(p.xyw, c.r) : vec4(c.r, p.yzx);
+
+    float d = q.x - min(q.w, q.y);
+    float e = 1.0e-10;
+    return vec3(abs(q.z + (q.w - q.y) / (6.0 * d + e)), d / (q.x + e), q.x);
+}
+
+vec3 HSVtoRGB(vec3 c)
+{
+    vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+    vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+    return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+}
 
 // conversion from NTSC RGB Reference White D65 ( color space used by NA/Japan TV's ) to XYZ
  vec3 NTSC(vec3 c)
